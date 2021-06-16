04fa9bc18128b50136928a49ae722914d2836eae
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-16 17:53:05 +0200

    add es 1

--- js/script.js
+++ js/script.js
@@ -17,24 +17,21 @@ const bici = [
 ];
 
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
-let biciLeggera;
-for (let i=0; i<bici.length; i++){
-    let {nome, peso} = bici[i];
+let biciLeggera = bici[0];
 
-    // console.log(nome, peso);
-    console.log(peso);
-
-    // biciLeggera = peso[0];
-    // console.log(biciLeggera);
-
-    // if(biciLeggera > peso[i]){
-    //     biciLeggera = peso[i]
-    // }
+for (let i=1; i<bici.length; i++){
+    if (biciLeggera.peso > bici[i].peso){
+        biciLeggera = bici[i]
+    }
 }
 // console.log(biciLeggera);
 
-
-
+const {nome, peso} = biciLeggera; //destructuring
+console.log( /*literal*/
+    `
+    La bici che pesa meno è la ${nome} che pesa ${peso} kg.
+    `
+);
 
 
 // Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (Utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti. Stampiamo tutto in console.
